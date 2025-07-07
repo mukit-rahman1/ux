b068ee563e25897f212f648766b56b1c0bba90ef
Daniel Vargas <daniel.vargas@live.ca>
2025-07-06 23:07:46 -0400

    Made mini navbar on team page functional with conditional rendering

--- uxWeb/src/pages/team.jsx
+++ uxWeb/src/pages/team.jsx
@@ -12,9 +12,51 @@ function Team() {
         {label: "Website", value: "website"}
     ];
 
+    const teamData = {
+        cofounders: [
+            {name: "Alice", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+            {name: "Gabe", role: "Executive", image: "/image-url-here"}
+        ],
+        sponsorships: [
+            {name: "Eli", role: "Director", image: "/image-url-here"},
+            {name: "Fay", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+        ],
+        marketing: [
+            {name: "Eli", role: "Director", image: "/image-url-here"},
+            {name: "Fay", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"}
+        ],
+        events: [
+            {name: "Eli", role: "Director", image: "/image-url-here"},
+            {name: "Fay", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+        ],
+        hackathon: [
+            {name: "Eli", role: "Director", image: "/image-url-here"},
+            {name: "Fay", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+        ],
+        website: [
+            {name: "Alice", role: "Executive", image: "/image-url-here"},
+            {name: "John", role: "Executive", image: "/image-url-here"},
+            {name: "Holly", role: "Executive", image: "/image-url-here"},
+            {name: "Gabe", role: "Executive", image: "/image-url-here"}
+        ]
+
+
+    }
     
     return (
-        <div> 
+        <div className="min-h-[1330px]"> 
             <div className="mb-[33px]">
                 <h1 className="page-title">Our Team</h1>
                 <p className="red-text">Meet the amazing people behind QUX</p>
@@ -31,7 +73,34 @@ function Team() {
                     ))
                 }
             </div>
-            <div>
+            <div className="mt-[33px]">
+                {(() => {
+                    const director = teamData[selected].find((member) => member.role === "Director");
+                    return director ? (
+                        <div className="member-container mb-[66.93px]">
+                            <div className="member-border"><div className="member-card"></div></div>
+                            <div className="member-textbox"> 
+                                <h3 className="text-[20px] font-[600] h-[27px]">{director.name}</h3>
+                                <p className="member-role">{director.role}</p>
+                            </div>
+                        </div>
+                    ) : null;
+                })()}
+
+                <div className={`executive-row ${teamData[selected].length == 4 ? "w-[50%]" : "w-[65%]"}`}>
+                    {teamData[selected].map((member, index) => (
+                        member.role != "Director" && (
+                            <div className="member-container" key={index}>
+                                <div className="member-border"><div className="member-card"></div></div>
+                                <div className="member-textbox"> 
+                                    <h3 className="text-[20px] font-[600] h-[27px]">{member.name}</h3>
+                                    <p className="member-role">{member.role}</p>
+                                </div>
+                            </div>
+                        )
+                        
+                    ))}
+                </div>
 
             </div>
         </div>
