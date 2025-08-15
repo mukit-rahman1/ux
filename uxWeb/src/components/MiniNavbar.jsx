e02810742815de2b030c7ff2e0cbe0601426e229
Daniel Vargas <daniel.vargas@live.ca>
2025-08-15 18:34:58 -0400

    Fixed mini navbar font and sizing issue

--- uxWeb/src/components/MiniNavbar.jsx
+++ uxWeb/src/components/MiniNavbar.jsx
@@ -6,7 +6,7 @@ export default function MiniNavbar({ options, setSelected, selected }) {
                     options.map((option) => (
                         <button 
                         key={option.value} 
-                        className={`mini-navbar-option text-[clamp(8px,1vw,18px)] ${selected == option.value && 
+                        className={`mini-navbar-option ${selected == option.value && 
                             ("bg-gradient-to-r from-[#8FB1BE] via-[#00022B] to-[#D53302] text-white ")}`} 
                         onClick={() => setSelected(option.value)}>
                             {option.label}
